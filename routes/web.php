f281788cfb7eab0bf9c32b08b4bfb8fcd48924de
Miguel Ceballos <mceballosleonides@gmail.com>
2025-02-02 01:01:37 -0600

    instala jetstream

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,13 @@ Route::get('/', function () {
 });
 
 Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
