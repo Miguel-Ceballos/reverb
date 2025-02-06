73b46c23eae63c569ca782e03ec5b53dbcdab4c6
Miguel Ceballos <mceballosleonides@gmail.com>
2025-02-05 21:34:12 -0600

    feat: crea formulario

---
+++ resources/views/livewire/manage-messages.blade.php
@@ -0,0 +1,52 @@
+<div>
+    <form class="min-w-full divide-y divide-gray-200" wire:submit.prevent="save">
+        <div class="mb-4">
+            <x-label for="message" value="Message" />
+            <x-textarea wire:model="content" class="w-full mt-2" placeholder="Type your message here..." />
+            <x-input-error for="content"/>
+        </div>
+
+        <div class="flex justify-end">
+            <x-button wire:loading.attr="disabled" class="ml-3">
+                Send
+            </x-button>
+        </div>
+    </form>
+
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 bg-white border-b border-gray-200">
+                <div class="flex items-center justify-between">
+                    <div class="flex-1 min-w-0">
+                        <h2 class="text-lg font-medium leading-6 text-gray-900">
+                            Messages
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            @foreach ($messages as $message)
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="flex items-center justify-between">
+                        <div class="flex min-w-0 gap-2">
+                            <div class="text-sm font-bold text-gray-900">
+                                {{ '@' . $message->user->name }}
+                            </div>
+                            -
+                            <div class="text-sm text-gray-500">
+                                {{ $message->user->email }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-2 flex items-center justify-between">
+                        <div class="text-sm">
+                            {{ $message->content }}
+                        </div>
+                        <div class="text-sm text-gray-500">
+                            {{ $message->created_at }}
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -18,58 +18,60 @@
                     </div>
                 </div>
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Name
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Email
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Date
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Action
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach ($messages as $message)
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="flex items-center">
-                                            <div class="flex-shrink-0 h-10 w-10">
-                                                <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                                            </div>
-                                            <div class="ml-4">
-                                                <div class="text-sm font-medium text-gray-900">
-                                                    {{ $message->user->name }}
-                                                </div>
-                                                <div class="text-sm text-gray-500">
-                                                    {{ $message->user->email }}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap">
-                                        <div class="text-sm text-gray-900">
-                                            {{ $message->content }}
-                                        </div>
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                        {{ $message->created_at }}
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">
-                                            View
-                                        </a>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+
+                    <livewire:manage-messages />
+{{--                    <table class="min-w-full divide-y divide-gray-200">--}}
+{{--                        <thead class="bg-gray-50">--}}
+{{--                            <tr>--}}
+{{--                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">--}}
+{{--                                    Name--}}
+{{--                                </th>--}}
+{{--                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">--}}
+{{--                                    Email--}}
+{{--                                </th>--}}
+{{--                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">--}}
+{{--                                    Date--}}
+{{--                                </th>--}}
+{{--                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">--}}
+{{--                                    Action--}}
+{{--                                </th>--}}
+{{--                            </tr>--}}
+{{--                        </thead>--}}
+{{--                        <tbody class="bg-white divide-y divide-gray-200">--}}
+{{--                            @foreach ($messages as $message)--}}
+{{--                                <tr>--}}
+{{--                                    <td class="px-6 py-4 whitespace-nowrap">--}}
+{{--                                        <div class="flex items-center">--}}
+{{--                                            <div class="flex-shrink-0 h-10 w-10">--}}
+{{--                                                <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">--}}
+{{--                                            </div>--}}
+{{--                                            <div class="ml-4">--}}
+{{--                                                <div class="text-sm font-medium text-gray-900">--}}
+{{--                                                    {{ $message->user->name }}--}}
+{{--                                                </div>--}}
+{{--                                                <div class="text-sm text-gray-500">--}}
+{{--                                                    {{ $message->user->email }}--}}
+{{--                                                </div>--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                    </td>--}}
+{{--                                    <td class="px-6 py-4 whitespace-nowrap">--}}
+{{--                                        <div class="text-sm text-gray-900">--}}
+{{--                                            {{ $message->content }}--}}
+{{--                                        </div>--}}
+{{--                                    </td>--}}
+{{--                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">--}}
+{{--                                        {{ $message->created_at }}--}}
+{{--                                    </td>--}}
+{{--                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">--}}
+{{--                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">--}}
+{{--                                            View--}}
+{{--                                        </a>--}}
+{{--                                    </td>--}}
+{{--                                </tr>--}}
+{{--                            @endforeach--}}
+{{--                        </tbody>--}}
+{{--                    </table>--}}
                 </div>
             </div>
         </div>
